8e12c89a7570a0beb15a2da326b35984b76720ba
duoli <zhaody901@126.com>
2021-01-25 14:50:59 +0800

    检测是否是内网IP

--- src/Helper/EnvHelper.php
+++ src/Helper/EnvHelper.php
@@ -199,4 +199,26 @@ class EnvHelper
 
         return $os;
     }
+
+    /**
+     * IP 是否是内网地址
+     * @param string $ip
+     * @return bool
+     */
+    public static function isInternalIp(string $ip):bool
+    {
+        $ip = ip2long($ip);
+        if (!$ip) {
+            return false;
+        }
+        $net_local = ip2long('127.255.255.255') >> 24; //127.x.x.x
+        $net_a     = ip2long('10.255.255.255') >> 24; //A类网预留ip的网络地址
+        $net_b     = ip2long('172.31.255.255') >> 20; //B类网预留ip的网络地址
+        $net_c     = ip2long('192.168.255.255') >> 16; //C类网预留ip的网络地址
+        return
+            $ip >> 24 === $net_local ||
+            $ip >> 24 === $net_a ||
+            $ip >> 20 === $net_b ||
+            $ip >> 16 === $net_c;
+    }
 }
