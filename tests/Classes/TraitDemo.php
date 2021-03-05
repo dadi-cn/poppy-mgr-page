09038ba6e11bfb1c6545de0650c401001d8ef38d
duoli <zhaody901@126.com>
2021-03-05 16:51:46 +0800

    fix:身份证认证以及错误设置

--- tests/Classes/TraitDemo.php
+++ tests/Classes/TraitDemo.php
@@ -1,4 +1,8 @@
-<?php namespace Poppy\Framework\Tests\Classes;
+<?php
+
+declare(strict_types = 1);
+
+namespace Poppy\Framework\Tests\Classes;
 
 use Exception;
 use Poppy\Framework\Classes\Traits\AppTrait;
@@ -9,27 +13,27 @@ class TraitDemo
 {
     use AppTrait, KeyParserTrait;
 
-    public function error()
+    public function error(): bool
     {
         return $this->setError('This is an Error String');
     }
 
-    public function exception()
+    public function exception(): bool
     {
         return $this->setError((new Exception('This is an Exception String')));
     }
 
-    public function exceptionWithCode($code)
+    public function exceptionWithCode($code): bool
     {
         return $this->setError((new ApplicationException('This is an Exception With Code', $code)));
     }
 
-    public function success()
+    public function success(): bool
     {
         return $this->setSuccess('操作成功');
     }
 
-    public function successWithEmpty()
+    public function successWithEmpty(): bool
     {
         return true;
     }
