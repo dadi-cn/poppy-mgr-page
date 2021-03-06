8fff7601c8e598fc3c6061f803936414b6a969df
duoli <zhaody901@126.com>
2021-03-06 11:50:45 +0800

    Rbac Helper~

--- src/Rbac/Helper/RbacHelper.php
+++ src/Rbac/Helper/RbacHelper.php
@@ -1,7 +1,6 @@
 <?php namespace Poppy\Core\Rbac\Helper;
 
 use Illuminate\Database\Eloquent\Collection;
-use Poppy\Core\Models\PamPermission;
 
 /**
  * Class RbacHelper
@@ -10,15 +9,16 @@ class RbacHelper
 {
     /**
      * 获取权限以及分组
-     * @param string $account_type 账号类型
+     * @param string $type 账号类型
      * @return Collection
      */
-    public static function permission($account_type)
+    public static function permission(string $type): Collection
     {
-        $permission = PamPermission::where('account_type', $account_type)->get();
-        $collection = new Collection($permission);
+        $permissionClass = config('poppy.core.rbac.permission');
+        $permission      = (new $permissionClass)->where('type', $type)->get();
+        $collection      = new Collection($permission);
 
-        return $collection->groupBy('permission_group');
+        return $collection->groupBy('group');
     }
 }
 
