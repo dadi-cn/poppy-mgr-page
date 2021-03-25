77cbd997bed930982ce6640022cf872bcf6c0c80
duoli <zhaody901@126.com>
2021-03-25 23:26:18 +0800

    管理平台样式处理

--- poppy/mgr-page/resources/views/backend/tpl/_style.blade.php
+++ poppy/mgr-page/resources/views/backend/tpl/_style.blade.php
@@ -1,3 +1,10 @@
+<?php
+$_type = $_type ?? [];
+?>
 {!! Html::style('assets/libs/layui/css/layui.css') !!}
-{!! Html::style('assets/libs/easy-web/module/admin.css') !!}
+
 {!! Html::style('assets/libs/boot/style.css') !!}
+
+@if(!in_array('!easy-web', $_type))
+    {!! Html::style('assets/libs/easy-web/module/admin.css') !!}
+@endif
