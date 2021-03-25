c0b79dbedfd0e9c74862af9a1d23ec36515b837e
duoli <zhaody901@126.com>
2021-03-26 01:51:17 +0800

    更新progress

--- poppy/mgr-page/resources/views/tpl/progress.blade.php
+++ poppy/mgr-page/resources/views/tpl/progress.blade.php
@@ -1,35 +1,35 @@
 @extends('py-mgr-page::tpl.dialog')
 @section('dialog-main')
     @if ($total > 0)
-        <div class="alert alert-info">本次需要更新 <strong>{{$total}}</strong> 条信息, 每批次更新 <strong>{{$section}}</strong> 条, 还剩余
-            <strong>{{$left}}</strong>条
+        <div class="layui-elem-quote mt10">
+            @if ($left === 0)
+                <p>操作完成</p>
+            @else
+                <p>本次需要操作 <strong>{{$total}}</strong> 条数据, 每批次更新 <strong>{{$section}}</strong> 条, 还剩余
+                    <strong>{{$left}}</strong>条</p>
+            @endif
         </div>
         @if (isset($info))
-            <div class="alert alert-warning">{!! $info !!}</div>
+            <div class="layui-elem-quote mt10">{!! $info !!}</div>
         @endif
-        <div class="progress">
-            <div class="progress-bar bg-success progress-bar-striped text-warning"
-                 role="progressbar" aria-valuenow="{{$percentage}}" aria-valuemin="0" aria-valuemax="100"
-                 style="width:{{$percentage}}%">
-                {{$percentage}}%
-            </div>
+        <div class="layui-progress layui-progress-big" lay-showPercent="yes">
+            <div class="layui-progress-bar layui-bg-green" lay-percent="{{$percentage}}%"></div>
         </div>
-
+        <script>
+        layui.element.render();
+        </script>
         @if ($left === 0)
-            <div class="alert alert-success">更新成功</div>
             <script>
-			// over progress close it
-			Util.splash({
-				status : 0,
-				msg    : '更新成功!'
-			});
+            Util.splash({status : 0, message : '操作成功!'});
             </script>
         @else
             <script>
-			setTimeout("window.location.href = '{!!$continue_url!!}'", {{$continue_time}});
+            setTimeout("window.location.href = '{!!$continue_url!!}'", {{$continue_time}});
             </script>
         @endif
     @else
-        <div class="alert alert-warning">没有需要更新的内容</div>
+        <div class="layui-elem-quote">
+            <p>没有需要更新的内容</p>
+        </div>
     @endif
 @endsection
