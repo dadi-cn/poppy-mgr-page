18f9cc071711a1f9e8c202213213e18de403befd
duoli <zhaody901@126.com>
2021-03-26 14:26:38 +0800

    dialog - 后台~

--- resources/views/backend/tpl/dialog.blade.php
+++ resources/views/backend/tpl/dialog.blade.php
@@ -3,7 +3,7 @@
 @section('description', $_description ?? '')
 @section('head-content')
     @include('py-mgr-page::tpl._js_css', [
-        '_type' => ['layui', 'easy-web']
+        '_type' => ['layui-all']
     ])
 @endsection
 @section('body-main')
