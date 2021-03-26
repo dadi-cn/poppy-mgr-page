63964a6e00c2e9157026fa8fc604cc2e6a11a7c4
duoli <zhaody901@126.com>
2021-03-26 12:35:10 +0800

    Progress 进度更新完成~

--- poppy/mgr-page/resources/views/tpl/progress.blade.php
+++ poppy/mgr-page/resources/views/tpl/progress.blade.php
@@ -1,27 +1,29 @@
 @extends('py-mgr-page::tpl.dialog')
-@section('dialog-main')
+@extends('py-mgr-page::tpl.default')
+@section('head-css')
+    {!! Html::style('assets/libs/layui/css/layui.css') !!}
+@endsection
+@section('body-main')
+    @include('py-mgr-page::tpl._toastr')
     @if ($total > 0)
-        <div class="layui-elem-quote mt10">
+        <div class="layui-elem-quote mt10" style="{!! $left === 0 ? 'color:green' : ''!!}">
             <p>
                 本次需要操作 <strong>{{$total}}</strong> 条数据, 每批次更新 <strong>{{$section}}</strong> 条, 还剩余
                 <strong>{{$left}}</strong>条
-                @if ($left === 0) 操作完成 @endif
+                @if ($left === 0)
+                    操作完成
+                @endif
             </p>
         </div>
         @if (isset($info))
             <div class="layui-elem-quote mt10">{!! $info !!}</div>
         @endif
-        <div class="layui-progress layui-progress-big" lay-showPercent="yes">
-            <div class="layui-progress-bar layui-bg-green" lay-percent="{{$percentage}}%"></div>
+        <div class="layui-progress layui-progress-big">
+            <div class="layui-progress-bar layui-bg-green" style="width: {{$percentage}}%">
+                <span class="layui-progress-text">{{$percentage}}%</span>
+            </div>
         </div>
-        <script>
-        layui.element.render();
-        </script>
-        @if ($left === 0)
-            <script>
-            Util.splash({status : 0, message : '操作成功!'});
-            </script>
-        @else
+        @if ($left !== 0)
             <script>
             setTimeout("window.location.href = '{!!$continue_url!!}'", {{$continue_time}});
             </script>
