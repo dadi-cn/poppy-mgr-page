2f33b1c59d14b890eb4f3c87daac7d650eacaa88
duoli <zhaody901@126.com>
2021-04-13 10:51:24 +0800

    增加 无需列表函数

--- poppy/core/tests/Redis/RdsSetTest.php
+++ poppy/core/tests/Redis/RdsSetTest.php
@@ -2,29 +2,211 @@
 
 namespace Poppy\Core\Tests\Redis;
 
+use stdClass;
+
 class RdsSetTest extends RdsBaseTest
 {
-    private static $key = 'rds-set';
 
     /**
      * 能添加数组和字串/ID, 并且ID 和字串相同时候被视为一个值
      */
     public function testSAdd()
     {
-
-        if ($this->rds->sismember(self::$key, 1)) {
-            $remNum = $this->rds->srem(self::$key, 1);
+        $key = $this->key('s-add');
+        if ($this->rds->sismember($key, 1)) {
+            $remNum = $this->rds->srem($key, 1);
             $this->assertEquals(1, $remNum);
         }
 
-        $result = $this->rds->sadd(self::$key, 1);
-        $this->assertEquals(true, $result);
+        $result = $this->rds->sadd($key, 1);
+        $this->assertEquals(1, $result);
 
-        $result = $this->rds->sadd(self::$key, '1');
-        $this->assertEquals(false, $result);
+        $result = $this->rds->sadd($key, '1');
+        $this->assertEquals(0, $result);
 
         // add array
-        $this->rds->sadd(self::$key, ['1', '2', '3', 4]);
+        $result = $this->rds->sadd($key, ['1', '2', '3', 4]);
+        $this->assertEquals(3, $result);
+
+        $num = $this->rds->sadd($key, [new stdClass()]);
+        $this->assertEquals(1, $num);
+        $this->rds->del($key);
+    }
+
+    public function testSIsMember()
+    {
+        $key = $this->key('s-is-member');
+        $this->rds->sAdd($key, [
+            1, '1', [1], new stdClass(),
+        ]);
+        $res = $this->rds->sIsMember($key, [1]);
+        $this->assertTrue($res);
+        $res = $this->rds->sIsMember($key, '1');
+        $this->assertTrue($res);
+        $res = $this->rds->sIsMember($key, 1);
+        $this->assertTrue($res);
+        $res = $this->rds->sIsMember($key, new stdClass());
+        $this->assertTrue($res);
+        $this->rds->del($key);
+    }
+
+    public function testSPop()
+    {
+        $key = $this->key('s-pop');
+        $this->rds->sAdd($key, [
+            1, '1', [1], new stdClass(),
+        ]);
+        $res = $this->rds->sPop($key);
+        $this->assertNotEmpty($res);
+        $res = $this->rds->sPop($key);
+        $this->assertNotEmpty($res);
+        $res = $this->rds->sPop($key);
+        $this->assertNotEmpty($res);
+        $res = $this->rds->sPop($key);
+        $this->assertEmpty($res);
+        $this->rds->del($key);
+    }
+
+
+    public function testSRem()
+    {
+        $key = $this->key('s-rem');
+        $this->rds->sAdd($key, [
+            1, '1', [1], new stdClass(),
+        ]);
+        $res = $this->rds->sRem($key, 1);
+        $this->assertEquals(1, $res);
+        $res = $this->rds->sRem($key, [[1]]);
+        $this->assertEquals(1, $res);
+        $res = $this->rds->sRem($key, new stdClass());
+        $this->assertEquals(1, $res);
+        $res = $this->rds->sRem($key, '1');
+        $this->assertEquals(0, $res);
+        $this->rds->del($key);
+    }
+
+
+    public function testSMove()
+    {
+        $key     = $this->key('s-move');
+        $keyDist = $this->key('s-move-dist');
+        $this->rds->sAdd($key, [
+            1, '1', [1], new stdClass(),
+        ]);
+        $res = $this->rds->sMove($key, $keyDist, 1);
+        $this->assertEquals(1, $res);
+        $res = $this->rds->sMove($key, $keyDist, [1]);
+        $this->assertEquals(1, $res);
+        $res = $this->rds->sMove($key, $keyDist, new stdClass());
+        $this->assertEquals(1, $res);
+        $res = $this->rds->scard($keyDist);
+        $this->assertEquals(3, $res);
+        $res = $this->rds->scard($key);
+        $this->assertEquals(0, $res);
+        $this->rds->del([$key, $keyDist]);
+    }
+
+    public function testCard()
+    {
+        $key = $this->key('s-card');
+        $res = $this->rds->sCard($key);
+        $this->assertEquals(0, $res);
+        $this->rds->sAdd($key, [
+            1, '1', [1], new stdClass(),
+        ]);
+
+        $res = $this->rds->sCard($key);
+        $this->assertEquals(3, $res);
+    }
+
+
+    public function testSMembers()
+    {
+        $key = $this->key('s-members');
+        $res = $this->rds->sMembers($key);
+        $this->assertEquals([], $res);
+        $this->rds->sAdd($key, [
+            1, '1', [1], new stdClass(),
+        ]);
+
+        $res = $this->rds->sMembers($key);
+        $this->assertCount(3, $res);
+        $this->rds->del($key);
+    }
+
+    public function testSScan()
+    {
+        $key = $this->key('s-scan');
+        $this->rds->sAdd($key, [
+            1, '1', [1], new stdClass(),
+        ]);
+        $request = true;
+        $result  = [];
+        $cursor  = 0;
+        while ($request) {
+            // 使用循环来获取数据, 不支持排序(无序列表)
+            $val    = $this->rds->sScan($key, $cursor, [
+                'count' => 1,
+            ]);
+            $result = array_merge($result, $val[1]);
+            if ($val[0]) {
+                $cursor = $val[0];
+            }
+            else {
+                $this->assertCount(3, $result);
+                $request = false;
+            }
+        }
+        $this->rds->del($key);
+    }
+
+
+    public function testInter()
+    {
+        $key = $this->key('s-inter');
+        $this->rds->sAdd($key, [
+            1, '1', [1], new stdClass(),
+        ]);
+        $key2 = $this->key('s-inter-2');
+        $this->rds->sAdd($key2, [
+            [1],
+        ]);
+        $inter = $this->rds->sInter([
+            $key2, $key,
+        ]);
+        $this->assertTrue(in_array([1], $inter));
+
+
+        $keyStore = $this->key('s-inter-store');
+        $this->rds->sInterStore($keyStore, [
+            $key, $key2,
+        ]);
+        $this->assertCount(1, $this->rds->sMembers($keyStore));
+    }
+
+
+    public function testSUnion()
+    {
+        $key = $this->key('s-union');
+        $this->rds->sAdd($key, [
+            1, "1",
+        ]);
+        $key2 = $this->key('s-union-2');
+        $this->rds->sAdd($key2, [
+            [1],
+        ]);
+
+        $values = $this->rds->sUnion([
+            $key, $key2,
+        ]);
+        $this->assertTrue(in_array([1], $values));
+        $this->assertTrue(in_array(1, $values));
+
+        $keyStore = $this->key('s-union-store');
+        $this->rds->sUnionStore($keyStore, [
+            $key, $key2,
+        ]);
+        $this->assertCount(2, $this->rds->sMembers($keyStore));
     }
 
     /**
@@ -32,19 +214,29 @@ class RdsSetTest extends RdsBaseTest
      */
     public function testSDiff()
     {
-        $diffKey = self::$key . '-diff';
-        $this->rds->sadd($diffKey, 1);
-        $this->rds->sadd(self::$key, ['1', '2', '3', 4]);
-        $result = $this->rds->sdiff([self::$key, $diffKey]);
+        $key = $this->key('s-diff');
+        $this->rds->sadd($key, 1);
+
+        $key2 = $this->key('s-diff-2');
+        $this->rds->sadd($key2, ['1', '2', '3', 4]);
+
+        $result = $this->rds->sDiff([$key2, $key]);
         $this->assertEquals(['2', '3', '4'], $result);
+
+        $keyStore = $this->key('s-diff-store');
+        $this->rds->sDiffStore($keyStore, [
+            $key2, $key,
+        ]);
+
+        $this->assertCount(3, $this->rds->sMembers($keyStore));
     }
 
 
     public function testSRandMember()
     {
-        $randKey = self::$key . '-rand';
-        $this->rds->sadd($randKey, range(1, 20));
-        $rand = $this->rds->srandmember($randKey, 3);
+        $key = $this->key('s-diff');
+        $this->rds->sadd($key, range(1, 20));
+        $rand = $this->rds->srandmember($key, 3);
         $this->assertCount(3, $rand);
     }
 }
