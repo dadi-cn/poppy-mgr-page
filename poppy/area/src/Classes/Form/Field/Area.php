788b4c3c2ea5066aea0f813031af226386496bf3
duoli <zhaody901@126.com>
2021-04-26 17:41:40 +0800

    地区增加缓存

--- poppy/area/src/Classes/Form/Field/Area.php
+++ poppy/area/src/Classes/Form/Field/Area.php
@@ -3,7 +3,6 @@
 namespace Poppy\Area\Classes\Form\Field;
 
 use Poppy\Area\Models\PyArea;
-use Poppy\Framework\Helper\TreeHelper;
 use Poppy\System\Classes\Form\Field;
 
 final class Area extends Field
@@ -16,11 +15,8 @@ final class Area extends Field
 
     public function render()
     {
-        $items = PyArea::selectRaw("id,title,parent_id")->where('level', '<', 4)->get()->keyBy('id')->toArray();
-        $Tree  = new TreeHelper();
-        $Tree->init($items, 'id', 'parent_id', 'title');
         $this->addVariables([
-            'area' => $Tree->getTreeArray(0),
+            'area' => PyArea::cityTree(),
         ]);
         return parent::render();
     }
--- poppy/area/src/Classes/Grid/Filter/Presenter/Area.php
+++ poppy/area/src/Classes/Grid/Filter/Presenter/Area.php
@@ -3,7 +3,6 @@
 namespace Poppy\Area\Classes\Grid\Filter\Presenter;
 
 use Poppy\Area\Models\PyArea;
-use Poppy\Framework\Helper\TreeHelper;
 use Poppy\System\Classes\Grid\Filter\Presenter\Presenter;
 
 class Area extends Presenter
@@ -15,11 +14,8 @@ class Area extends Presenter
 
     public function variables(): array
     {
-        $items = PyArea::selectRaw("id,title,parent_id")->where('level', '<', 4)->get()->keyBy('id')->toArray();
-        $Tree  = new TreeHelper();
-        $Tree->init($items, 'id', 'parent_id', 'title');
         return [
-            'area' => $Tree->getTreeArray(0),
+            'area' => PyArea::cityTree(),
         ];
     }
 }
