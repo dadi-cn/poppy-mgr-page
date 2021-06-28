1836d311fae6cb332643bc5eb60263a067c215d1
duoli <zhaody901@126.com>
2021-06-28 18:03:29 +0800

    task # Url 解析

--- modules/php/tests/Other/UrlTest.php
+++ modules/php/tests/Other/UrlTest.php
@@ -15,4 +15,11 @@ class UrlTest extends SystemTestCase
         $string = urldecode(http_build_query($params));
         $this->assertEquals('a[0]=b&a[1]=c&a[2]=d', $string);
     }
+
+    public function testParseUrl()
+    {
+        $url = 'https://test-oss.aliyun.com/uploads/date/time/file.png';
+        $parsed = parse_url($url);
+        $this->assertEquals('/uploads/date/time/file.png', $parsed['path']);
+    }
 }
