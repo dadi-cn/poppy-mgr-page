3109d3cb2544cac14ae9dbaff352910287779420
duoli <zhaody901@126.com>
2021-08-31 15:33:38 +0800

    加入图片上传成功的验证, 修改单元测试的错误验证

--- tests/Action/StsTest.php
+++ tests/Action/StsTest.php
@@ -2,6 +2,8 @@
 
 namespace Poppy\AliyunOss\Tests\Action;
 
+use OSS\Core\OssException;
+use OSS\OssClient;
 use Poppy\AliyunOss\Action\Sts;
 use Poppy\System\Tests\Base\SystemTestCase;
 
@@ -18,6 +20,9 @@ class StsTest extends SystemTestCase
         $this->config = $this->readJson('poppy.aliyun-oss', 'tests/config/account.test.json');
     }
 
+    /**
+     * 测试授权KEY以及是否可以上传URL
+     */
     public function testTempKey()
     {
         $config = $this->config;
@@ -27,9 +32,30 @@ class StsTest extends SystemTestCase
             $temp = $Sts->getTempKey();
             $this->outputVariables($temp);
             $this->assertIsArray($temp);
+            $this->assertArrayHasKey("security_token", $temp);
+            $this->assertArrayHasKey("access_key_id", $temp);
+            $this->assertArrayHasKey("expiration", $temp);
+            // test upload
+
+            $accessKeyId     = $temp['access_key_id'];
+            $accessKeySecret = $temp['access_key_secret'];
+            $endpoint        = $config['endpoint'];
+
+            // 测试上传文件
+            try {
+                $ossClient = new OssClient($accessKeyId, $accessKeySecret, $endpoint, false, $temp['security_token']);
+                $url       = $temp['directory'] . '/demo.jpg';
+                $ossClient->uploadFile($config['bucket'], $url, poppy_path('poppy.aliyun-oss', 'tests/files/demo.jpg'));
+                $file = $config['url'] . '/' . $url;
+                $this->outputVariables($file);
+                $content = file_get_contents($file);
+                $this->assertGreaterThan(0, strlen($content));
+            } catch (OssException $e) {
+                print $e->getMessage();
+            }
         }
         else {
-            $this->assertTrue(false, $Sts->getError());
+            $this->fail($Sts->getError());
         }
     }
 }
