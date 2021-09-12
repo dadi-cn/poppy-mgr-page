82104b1077e795826c8a3792a4eb2eb4d4a2dbde
duoli <zhaody901@126.com>
2021-09-12 17:00:38 +0800

    remove py

--- py/aliyun-oss/src/Http/Routes/api_v1.php
+++
@@ -1,6 +0,0 @@
-<?php
-Route::group([
-    'namespace' => 'Poppy\AliyunOss\Http\Request\ApiV1\Web',
-], function (Illuminate\Routing\Router $route) {
-    $route->any('sts/temp_oss', 'StsController@tempOss');
-});
--- py/version/resources/lang/zh/seo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'backend_version_index'     => '版本管理',
-    'backend_version_establish' => '版本添加/编辑',
-    'backend_version_delete'    => '删除版本',
-];
