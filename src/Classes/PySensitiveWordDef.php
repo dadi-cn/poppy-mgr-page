2035938ec872d6d15aebd15a02e1c8d7194e7b59
duoli <zhaody901@126.com>
2021-09-12 16:58:55 +0800

    Squashed 'py/sensitive-word/' content from commit 0fbee646
    git-subtree-dir: py/sensitive-word
    git-subtree-split: 0fbee64661e466152d98934c3841e99a3b8e82c2

---
+++ src/Classes/PySensitiveWordDef.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Poppy\SensitiveWord\Classes;
+
+
+class PySensitiveWordDef
+{
+    /**
+     * 关键词字典
+     * @return string
+     */
+    public static function ckDict(): string
+    {
+        return 'dict';
+    }
+}
