f93db6029fdc9a9f992a8899984e6b61908b3452
duoli <zhaody901@126.com>
2021-10-08 17:08:53 +0800

    task # 后台管理失效图片添加, 需要重新发布下资源

--- resources/views/backend/home/index.blade.php
+++ resources/views/backend/home/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', $_title ?? '')
 @section('description', $_description ?? '')
 @section('head-meta')
-    {!! Html::favicon('assets/images/favicon.png') !!}
+    {!! Html::favicon('assets/images/default/favicon.png') !!}
 @endsection
 @section('head-content')
     @include('py-mgr-page::tpl._js_css', [
