c8ee4e1f67facb06650c392325c3e98b9c02975b
多厘 <zhaody901@126.com>
2022-03-13 18:07:07 +0800

    fix: 路由加载 controller 失败

--- poppy/mgr-page/src/Http/Request/Develop/CpController.php
+++ poppy/mgr-page/src/Http/Request/Develop/CpController.php
@@ -2,20 +2,6 @@
 
 namespace Poppy\MgrPage\Http\Request\Develop;
 
-use Carbon\Carbon;
-use Curl\Curl;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Response;
-use Illuminate\Routing\Redirector;
-use Illuminate\View\View;
-use Poppy\Framework\Classes\Resp;
-use Poppy\Framework\Helper\CookieHelper;
-use Poppy\System\Classes\Contracts\ApiSignContract;
-use Session;
-use Throwable;
-
 /**
  * 开发平台控制台 cp = ControlPanel
  */
