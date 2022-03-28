0476594bf207395f8330eba884fd36db2d71ba6c
多厘 <zhaody901@126.com>
2022-03-28 09:58:30 +0800

    feat: mgr-page 的消息提示

--- resources/views/tpl/message.blade.php
+++ resources/views/tpl/message.blade.php
@@ -12,42 +12,43 @@
         <div class="layui-col-md4 layui-col-md-offset4">
             <fieldset class="layui-elem-field layui-field-title">
                 <legend class="@if (Session::get('end.level') == 0 )  text-success @else text-danger  @endif">
-                    @if (Session::get('end.level') === 0 )
+                    @if ($code === 0 )
                         <h3 class="panel-title"><i class="fa fa-check-circle-o"></i> 提示</h3>
                     @endif
-                    @if (Session::get('end.level') === 1 )
+                    @if ($code === 1 )
                         <h3 class="panel-title"><i class="fa fa-times-circle-o"></i> 提示</h3>
                     @endif
                 </legend>
                 <div>
-                    <div class="pt15 pb15 @if (Session::get('end.level') == 0 )  text-success @else text-danger  @endif" >
-                        <p>{!! Session::get('end.message') !!}</p>
+                    <div class="pt15 pb15 @if ($code === 0 )  text-success @else text-danger  @endif">
+                        <p>{!! $message !!}</p>
                     </div>
-                    <p class="text-center">
-                        @if (isset($location))
-                            @if ($location == 'back' || $time == 0)
-                                @if ($location != 'message')
+                    @if (isset($to))
+                        <p class="text-center">
+                            @if ($to == 'back')
+                                @if ($to != 'message')
                                     <a href="javascript:window.history.go(-1);">返回上级</a>
                                 @endif
+                            @elseif(!$time)
+                                <meta http-equiv="refresh" content="0;URL='{!! $to !!}'"/>
                             @else
                                 您将在 <span id="clock">0</span>秒内跳转至目标页面, 如果不想等待, <a
-                                        href="{!! $location !!}">点此立即跳转</a>!
+                                        href="{!! $to !!}">点此立即跳转</a>!
                                 <script>
-								$(function() {
-									let t = {!! $time !!};//设定跳转的时间
-									setInterval(refer(), 1000); //启动1秒定时
-									function refer() {
-										if (t == 0) {
-											window.location.href = "{!! $location !!}"; //设定跳转的链接地址
-										}
-										$('#clock').text(Math.ceil(t / 1000)); // 显示倒计时
-										t -= 1000;
-									}
-								})
+                                $(function () {
+                                    let t = {!! $time !!};//设定跳转的时间
+                                    setInterval(function (){
+                                        if (t == 0) {
+                                            window.location.href = "{!! $to !!}"; //设定跳转的链接地址
+                                        }
+                                        $('#clock').text(Math.ceil(t / 1000)); // 显示倒计时
+                                        t -= 1000;
+                                    }, 1000); //启动1秒定时
+                                })
                                 </script>
                             @endif
-                        @endif
-                    </p>
+                        </p>
+                    @endif
                 </div>
             </fieldset>
         </div>
